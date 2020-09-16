2eeded9a6ff8e31de9d77ddcd611c8e809255913
Juan <satelimportadores@gmail.com>
2020-09-16 14:52:37 -0500

    Crear las rutas

--- Readme.txt
+++ Readme.txt
@@ -3,4 +3,7 @@ Iniciar el proyecto
     npm init -y
 
 Agregar dependencias:
-    npm i express mongoose multer dotenv cross-env cors fs-extra
+    npm i express mongoose morgan multer dotenv cross-env cors fs-extra
+        multer-dependencia espcecial para imagenes
+    Agregar dependencias de desarrollo:
+    npm i nodemon
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 
 //Inicializar
@@ -7,6 +10,24 @@ const app = express();
 
 //Configuraciones
 app.set('port',3000);
+
+//Middleware
+app.use(morgan('dev'));
+
+    //multer para subir imagenes:
+    const storage = multer.diskStorage({
+        destination: path.join(__dirname,'public/uploads'),
+        filename(req,file,cb){
+            cb(null, new Date().getTime()+ path.extname(file.originalname));
+        }
+    })
+app.use(multer({storage}).single('image'));
+
+    //ayudar a formularios a enviarlos como json
+app.use(express.urlencoded({extended: false}));
+
+    //enviar peticiones JSON
+app.use(express.json());
 
 //Iniciar el servidor
 app.listen(app.get('port'),()=>{
